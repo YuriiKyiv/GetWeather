01cc4974cc0a8eb3c04ac98e042a5d40b6d53ef4
Yurii Trokhymchuk <yurii.trokhymchuk@skylum.com>
2020-04-19 20:22:39 +0300

    Initial Commit

---
+++ GetWeather/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  GetWeather
+//
+//  Created by Yurii Trokhymchuk on 19.04.2020.
+//  Copyright © 2020 Yurii Trokhymchuk. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
